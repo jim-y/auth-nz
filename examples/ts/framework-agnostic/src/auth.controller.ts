46c52f73c670639e398cf96499d7eef2165b18d1
Attila Kling <attila.kling@gmail.com>
2020-12-05 21:07:53 +0100

    Added docusaurus to documentation branch

--- examples/ts/framework-agnostic/src/auth.controller.ts
+++ examples/ts/framework-agnostic/src/auth.controller.ts
@@ -1,10 +1,6 @@
 import { Controller, Get, Req, Render, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import {
-  atoms,
-  Request as AuthnzRequest,
-  AuthorizationRequestMeta,
-} from 'auth-nz';
+import { atoms, AuthorizationRequestMeta } from 'auth-nz';
 import { Request, Response } from 'express';
 
 @Controller()
@@ -21,6 +17,7 @@ export class AuthController {
     // Except the client, other information is ok to "leak"
     // In other examples the meta is stored in the session but this example
     // tries to use as less dependencies as it can
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { client, ...meta } = res.locals.authorizationServer;
     return { ...meta, payload: JSON.stringify(meta) };
   }
@@ -30,11 +27,11 @@ export class AuthController {
     const authorizationRequestMeta: AuthorizationRequestMeta = JSON.parse(
       req.body.meta,
     );
-    const { qs } = await atoms.AuthorizationRequest.validateAuthorizationCode(
+    const { qs } = (await atoms.AuthorizationRequest.validateAuthorizationCode(
       atoms.getRequest(req),
       this.authService.createAuthorizationCode.bind(this.authService),
       authorizationRequestMeta,
-    ) as any;
+    )) as any;
 
     res.redirect(`${authorizationRequestMeta.redirectUri}?${qs}`);
   }
