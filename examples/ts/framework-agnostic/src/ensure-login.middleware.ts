1e9767ab41d60fba22e9bb331354f4b57b08499e
Attila Kling <attila.kling@gmail.com>
2020-12-05 22:31:42 +0100

    Removed examples folder from master

--- examples/ts/framework-agnostic/src/ensure-login.middleware.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from 'express';
-
-export function ensureLogin(req: Request, res: Response, next: Function) {
-  // TODO implement user authentication using session or jwt-auth or whatever
-  next();
-}
